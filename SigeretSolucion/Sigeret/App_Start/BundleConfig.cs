4e7fcd86f40a84b31d4afc4e5c7ad0a51c57a951
saulhs <saulhs89@gmail.com>
2014-04-21 09:06:51 -0400

    Implementación de Bootstrap
    Implementación de Bootstrap para los estilos y conversión responsive del
    proyecto, además de la creación de un nuevo modelo de dominio para la
    manipulación y manejo más fácil.

--- SigeretSolucion/Sigeret/App_Start/BundleConfig.cs
+++ SigeretSolucion/Sigeret/App_Start/BundleConfig.cs
@@ -11,6 +11,10 @@ namespace Sigeret
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            // Agregando referencia de Boostrap para el Layaout de la aplicación -- Saúl H. S.
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/bootstrap.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
 
@@ -24,6 +28,9 @@ namespace Sigeret
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+
+            // Adicionando referencia para los estilos. -- Saúl H. S.
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
