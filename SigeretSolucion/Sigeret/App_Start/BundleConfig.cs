6a77fc65eeaa0a8c5917a7ebafb2d64834c1fa1f
Saúl H. S. <saulhs89@gmail.com>
2014-05-05 16:17:43 -0400

    estilos aplicados

--- SigeretSolucion/Sigeret/App_Start/BundleConfig.cs
+++ SigeretSolucion/Sigeret/App_Start/BundleConfig.cs
@@ -62,7 +62,8 @@ namespace Sigeret
                         "~/Content/themes/base/jquery.ui.tabs.css",
                         "~/Content/themes/base/jquery.ui.datepicker.css",
                         "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+                        "~/Content/themes/base/jquery.ui.theme.css",
+                        "~/Content/chosen.css"));
         }
     }
 }
