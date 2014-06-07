619045f78430b390f2a3264bbc29c11f4854122b
Saúl H. S. <saulhs89@gmail.com>
2014-06-06 21:56:41 -0400

    Equipo y Modelo Equipo completados

--- SigeretSolucion/Sigeret/Controllers/BaseController.cs
+++ SigeretSolucion/Sigeret/Controllers/BaseController.cs
@@ -2,6 +2,7 @@
 using Sigeret.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Resources;
 using System.Web;
@@ -90,6 +91,19 @@ namespace Sigeret.Controllers
                  httpContext, errorRoute));
 
             return new EmptyResult();
+        }
+
+        public string PartialViewToString(string viewName, object model)
+        {
+            ViewData.Model = model;
+            using (var sw = new StringWriter())
+            {
+                var viewResult = ViewEngines.Engines.FindPartialView(ControllerContext, viewName);
+                var viewContext = new ViewContext(ControllerContext, viewResult.View, ViewData, TempData, sw);
+                viewResult.View.Render(viewContext, sw);
+                viewResult.ViewEngine.ReleaseView(ControllerContext, viewResult.View);
+                return sw.GetStringBuilder().ToString();
+            }
         }
     }
 }
